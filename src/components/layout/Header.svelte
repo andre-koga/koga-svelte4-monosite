3b5734dc53109bd6ad58394e6c6912611ff173f9
S Taubman <akoga3@gatech.edu>
2024-07-22 22:05:13 +0200

    navbar done

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -10,6 +10,7 @@
 
 	// Function to toggle the theme
 	function toggleTheme() {
+		displayProjects = false;
 		theme.update((current) => {
 			const newTheme = current === 'light' ? 'dark' : 'light';
 			document.documentElement.setAttribute('data-theme', newTheme);
@@ -40,8 +41,42 @@
 	</button>
 </header>
 
-<aside class="relative z-10 px-6 sm:px-12 md:px-20 {displayProjects ? 'show-aside' : 'hide-aside'}">
-	hey!
+<aside
+	class="relative z-10 mx-6 mt-8 grid grid-cols-2 gap-4 overflow-hidden transition-all sm:mx-12 sm:grid-cols-4 md:mx-20 md:grid-cols-5 lg:grid-cols-6 {displayProjects
+		? 'show-aside'
+		: 'hide-aside'}"
+>
+	<div
+		class="flex flex-col gap-3 rounded-xl border border-darkbg border-opacity-20 bg-lightbg p-4 dark:border-lightbg dark:border-opacity-20 dark:bg-darkbg"
+	>
+		<p class="font-bold">Projects</p>
+		<projects class="flex flex-col items-start gap-1">
+			<a
+				style="background:linear-gradient(to right, #FFA981, #FF73AD, #FF54D0);"
+				class="rounded-lg px-2 text-lighttext transition-transform hover:scale-95"
+				href="https://th.andrekoga.com"
+			>
+				thoughts
+			</a>
+			<a
+				style="background:linear-gradient(to right, #DC4C3B, #EF8E2D, #FFB830);"
+				class="rounded-lg px-2 text-lighttext transition-transform hover:scale-95"
+				href="https://nu.andrekoga.com"
+			>
+				numerus
+			</a>
+		</projects>
+	</div>
+	<div
+		class="flex flex-col gap-3 rounded-xl border border-darkbg border-opacity-20 bg-lightbg p-4 dark:border-lightbg dark:border-opacity-20 dark:bg-darkbg"
+	>
+		<p class="font-bold">Links</p>
+		<links class="flex flex-col gap-1 opacity-70">
+			<a class="underline-offset-2 hover:underline" href="/resume"> resume </a>
+			<a class="underline-offset-2 hover:underline" href="/contact"> contact </a>
+			<a class="underline-offset-2 hover:underline" href="/about"> about </a>
+		</links>
+	</div>
 </aside>
 
 <gradient class="absolute left-0 top-0 h-6 w-full bg-lightbg dark:bg-darkbg"> </gradient>
@@ -51,12 +86,12 @@
 
 <style>
 	.show-aside {
-		opacity: 1;
+		@apply h-60 opacity-100;
 		pointer-events: auto;
 	}
 
 	.hide-aside {
-		opacity: 0;
+		@apply h-0 opacity-0;
 		pointer-events: none;
 	}
 </style>
