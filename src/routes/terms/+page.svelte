e1f30a6b6f4826c9f891b213de4ddbf0b21a3b27
S Taubman <akoga3@gatech.edu>
2024-08-22 16:34:36 -0400

    fixed some other pages

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,5 +1,10 @@
-<first-section class="spacing block text-justify">
-	<p class="spacing">
+<script>
+	import Header from '$layout/Header.svelte';
+</script>
+
+<Header />
+<first-section class="container mx-auto my-20 block text-justify">
+	<p>
 		<b>Hey there!</b> We're all about having fun here, so go ahead and enjoy exploring our site. Just
 		a couple of things to keep in mind: please be respectful and thoughtful with what you post. Remember,
 		you're responsible for your own actions and the content you share. Let's keep it cool, positive,
--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,31 +1,37 @@
-<first-section class="spacing block">
-	<p class="text-center text-sm lowercase italic">
+<script>
+	import Header from '$layout/Header.svelte';
+</script>
+
+<Header />
+
+<first-section class="container mx-auto my-20 block">
+	<p class="mb-10 text-center text-sm lowercase italic">
 		Below is a list of the tools and packages used for the creation of this website and the other
 		projects:
 	</p>
-	<ul class="spacing flex flex-wrap justify-center gap-4">
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+	<ul class="flex flex-wrap justify-center gap-4">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://svelte.dev/">Svelte 4</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://github.com/andre-koga/koga-svelte4-monosite">Github</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://code.visualstudio.com/">VS Code</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://gsap.com/">GSAP</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://www.figma.com/">Figma</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://firebase.google.com/">Firebase</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://tailwindcss.com/">Tailwind</a>
 		</li>
-		<li class="rounded-lg border border-darkbg px-2 dark:border-lightbg">
+		<li class=" border border-darkbg px-2 dark:border-lightbg">
 			<a href="https://prettier.io/">Prettier</a>
 		</li>
 	</ul>
